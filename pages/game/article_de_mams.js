c2d754f7765c7db1ba1c2aaed17deb61d6a43a6b
Matteo Leclercq _1 <matteo.ftt@gmail.com>
2024-06-29 17:13:28 +0200

    try github pages

--- pages/game/article_de_mams.js
+++ pages/game/article_de_mams.js
@@ -6,15 +6,17 @@ var images = [
     "../../assets/img/article_de_mams/article4.png",
     "../../assets/img/article_de_mams/article5.png",
     "../../assets/img/article_de_mams/article6.png",
-    "../../assets/img/article_de_mams/article7.png",
-    "../../assets/img/article_de_mams/article8.png",
-    "../../assets/img/article_de_mams/article9.png",
-    "../../assets/img/article_de_mams/article10.png",
 	"../../assets/img/end.png",
 ]
 
 var reponses = [
 	"",
+	"Vrai! \n 'Macron s hometown voters look set to shun the president'",
+	"Faux! C'est juste une image de poux",
+	"Vrai JAPANESE FILM FESTIVAL ONLINE 2024 en France",
+	"Vrai : Des « tiques géantes » porteuses d’un virus progressent en France, voici pourquoi c’est inquiétant",
+	"Faux : Les 10 principales contre-vérités du RN",
+	"Faux : McFly et Carlito: L’Histoire du Bad Buzz avec “TikTok Girl”",
 	"",
 ]
 
